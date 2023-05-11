f71e1cf48caa9a2f3a6dd278ccefb92c7b8796ed
StephanJ98 <diego.jeandon@gmail.com>
2023-05-11 03:00:22 +0200

    :pencil: Creation de quelques pages et composants

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div>
+      <Header/>
+      <p>Register</p>
+    </div>
+  )
+}
